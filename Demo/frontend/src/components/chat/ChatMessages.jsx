064ef95d105c1b729f68d7b647efd3948076e807
Ankit Jha <ankitjha62061@gmail.com>
2026-06-24 18:03:14 +0530

    Implement sent/received message alignment in chat UI

--- Demo/frontend/src/components/chat/ChatMessages.jsx
+++ Demo/frontend/src/components/chat/ChatMessages.jsx
@@ -1,15 +1,44 @@
 function ChatMessages({ messages }) {
   return (
+
+    
     <div className="flex-1 p-4 overflow-y-auto">
 
+{messages.map((msg, index) => (
+  <div
+    key={index}
+    className={`mb-2 ${
+      msg.sender === "me"
+        ? "text-right"
+        : "text-left"
+    }`}
+  >
+    <div
+      className={`inline-block p-2 rounded ${
+        msg.sender === "me"
+          ? "bg-blue-500 text-white"
+          : "bg-gray-200"
+      }`}
+    >
+      {msg.text}
+    </div>
+  </div>
+))}
+
+
+
+
+{/* 
       {messages.map((msg, index) => (
         <div
           key={index}
+
+          
           className="bg-gray-200 p-2 rounded w-fit mb-2"
         >
           {msg.text}
         </div>
-      ))}
+      ))} */}
 
     </div>
   );
